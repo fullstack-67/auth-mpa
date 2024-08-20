5028088335eed53268c522aa192a3897ac3bb5aa
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2024-08-20 14:31:26 +0700

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,47 @@
 import "dotenv/config";
 import express from "express";
-import cors from "cors";
-import helmet from "helmet";
-import morgan from "morgan";
-import sessionIns, {
-  setSessionInfoAfterLogin,
-  formatSession,
-} from "./auth/session.js";
-import passportIns from "./auth/passport.js";
-import * as useragent from "express-useragent";
-import { deleteSession, createUser } from "@db/repositories.js";
-import { PORT, NODE_ENV } from "./utils/env.js";
+import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
+import { dbClient } from "@db/client.js";
+import { usersTable } from "@db/schema.js";
+import { eq } from "drizzle-orm";
+import bcrypt from "bcrypt";
 
 const app = express(); // Intializing the express app
 app.set("view engine", "pug");
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json()); // Extracts the entire body portion of an incoming request stream and exposes it on req.body.
+app.use(express.json());
 app.use(express.static("public"));
-app.use(morgan("dev"));
-const scriptSources = ["'self'", "https://unpkg.com"];
-const styleSources = ["'self'", "'unsafe-inline'", "cdn.jsdelivr.net"];
-const connectSources = ["'self'"];
-app.use(
-  helmet({
-    contentSecurityPolicy: {
-      directives: {
-        scriptSrc: scriptSources,
-        scriptSrcElem: scriptSources,
-        styleSrc: styleSources,
-        connectSrc: connectSources,
-      },
-      reportOnly: true,
+// * Passort setup
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
     },
-  })
+    async function (email, password, done) {
+      const query = await dbClient.query.usersTable.findFirst({
+        where: eq(usersTable.email, email),
+      });
+      if (!query) done(null, false, { message: "No email exists" });
+      bcrypt.compare(password, query?.password ?? "", function (err, result) {
+        if (err) done(err, false);
+        if (result) {
+          return done(null, query);
+        } else {
+          return done(null, false, { message: "Incorrect Password" });
+        }
+      });
+    }
+  )
 );
-app.use(
-  cors({
-    origin: false, // Disable CORS
-    // origin: "*", // Allow all origins
-  })
-);
-app.use(useragent.express());
-
-// * Session
-if (NODE_ENV === "production") app.set("trust proxy", 1); // trust first proxy
-app.use(sessionIns);
-app.use(passportIns.initialize());
-app.use(passportIns.session());
+app.use(passport.initialize());
 
 // * Endpoints
 app.get("/", async (req, res, next) => {
-  console.log("----------/--------------");
-  console.dir({
-    session: req.session,
-    user: req.user,
-    sessionID: req.sessionID,
-  });
-  const sessions = await formatSession(req);
   res.render("pages/index", {
     title: "Home",
     user: req.user,
-    sessions: sessions,
   });
 });
 
@@ -70,8 +51,29 @@ app.get("/signup", function (req, res) {
   });
 });
 
+async function createUser(name: string, email: string, password: string) {
+  const saltRounds = 10;
+  let hashedPassword = "";
+  hashedPassword = await new Promise((resolve, reject) => {
+    bcrypt.hash(password, saltRounds, function (err, hash) {
+      if (err) reject(err);
+      resolve(hash);
+    });
+  });
+  await dbClient
+    .insert(usersTable)
+    .values({
+      name,
+      email,
+      isAdmin: false,
+      password: hashedPassword,
+      avatarURL: "logos/robot.png",
+    })
+    .returning({ id: usersTable.id });
+}
+
 app.post("/signup", async function (req, res) {
-  console.log(req.body);
+  // console.log(req.body);
   const name = req.body?.name ?? "";
   const email = req.body?.email ?? "";
   const password = req.body?.password ?? "";
@@ -96,65 +98,16 @@ app.get("/login", function (req, res) {
   });
 });
 
-app.post("/login", passportIns.authenticate("local"), function (req, res) {
-  console.log("----------Login--------------");
-  // console.log(req.body);
-  // console.log(req.session);
-  setSessionInfoAfterLogin(req, "CREDENTIAL");
-  res.setHeader("HX-Redirect", "/");
-  res.send(`<div></div>`);
-});
-
-app.get("/login/oauth/github", passportIns.authenticate("github"));
-
-app.get(
-  "/callback/github",
-  passportIns.authenticate("github", { failureRedirect: "/login" }),
+app.post(
+  "/login",
+  passport.authenticate("local", { session: false }),
   function (req, res) {
-    console.log("----------Callback--------------");
-    setSessionInfoAfterLogin(req, "GITHUB");
-    res.redirect("/");
+    res.send(`<span>Login Successfully</span>`);
   }
 );
-
-app.get("/login/oauth/google", passportIns.authenticate("google"));
-
-app.get(
-  "/callback/google",
-  passportIns.authenticate("google", { failureRedirect: "/login" }),
-  function (req, res) {
-    console.log("----------Callback--------------");
-    setSessionInfoAfterLogin(req, "GOOGLE");
-    res.redirect("/");
-  }
-);
-
-app.post("/logout", function (req, res, next) {
-  // req.logout will not delete the session in db. It will generate new one for the already-logout user.
-  // When the user login again, it will generate new session with the user id.
-  req.logout(function (err) {
-    if (err) {
-      return next(err);
-    }
-    // If you want to delete the session in DB, you can use this function.
-    req.session.destroy(function (err) {
-      if (err) {
-        return next(err);
-      }
-      res.setHeader("HX-Redirect", "/");
-      res.send("<div></div>");
-    });
-  });
-});
-
-app.delete("/session", async function (req, res, next) {
-  const sid = (req?.query?.sid ?? "") as string;
-  const request = await deleteSession(sid);
-  res.setHeader("HX-Redirect", "/");
-  res.send(`<div></div>`);
-});
 
 // * Running app
+const PORT = 5001;
 app.listen(PORT, async () => {
   console.log(`Listening on port ${PORT}`);
   console.log(`http://localhost:${PORT}`);
